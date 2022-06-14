2f75c2aaed39f3409902e9dc098dbaa3344f1156
Adel <adel.amine@spidex.co.uk>
2022-06-14 17:10:42 +0100

    initial commit

---
+++ src/main/java/com/okta/developer/jugtours/model/User.java
@@ -0,0 +1,22 @@
+package com.okta.developer.jugtours.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+}
